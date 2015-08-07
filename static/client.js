15a43526eacfbc5235e31c5c2c0d2fc0121b0761
Mikael Koponen <mikael.koponen@gmail.com>
2015-08-07 11:20:26 +0300

    Check current status on page load

--- static/client.js
+++ static/client.js
@@ -1,23 +1,37 @@
 $(function() {
   var fileName = window.location.hash.substring(1);
-  if (fileName) {
-    $("#download").removeClass("hidden").attr("href", downloadUrl(fileName));
-    $("#reload").removeClass("hidden");
+  if (fileName) { pollResponse(fileName); }
+
+  var loader = $("#loading");
+  function pollResponse(res) {
+    $.ajax("/status/" + res, {
+      statusCode: {
+        200: function() {
+          loader.addClass("hidden");
+          $("#download").attr("href", downloadUrl(res)).removeClass("hidden");
+          $("#reload").removeClass("hidden");
+        },
+        202: function() {
+          loader.removeClass('hidden');
+          setTimeout(function() { pollResponse(res); }, 1000);
+        },
+        500: function() {
+          loader.addClass("hidden");
+          $("#error").removeClass("hidden");
+        }
+      }
+    });
   }
 
   $('#upload').on('submit', function(event) {
     event.preventDefault();
-    var form = $(this);
-    var loader = $("#loading");
 
     loader.removeClass('hidden');
-
-    var data = new FormData(form[0]);
 
     $.ajax({
       url: '/upload',
       type: 'POST',
-      data: data,
+      data: new FormData($(this)[0]),
       cache: false,
       processData: false,
       contentType: false
@@ -26,25 +40,8 @@ $(function() {
       pollResponse(res);
     });
 
-    function pollResponse(res) {
-      $.ajax("/status/" + res, {
-        statusCode: {
-          200: function() {
-            loader.addClass("hidden");
-            $("#download").attr("href", downloadUrl(res)).removeClass("hidden");
-            $("#reload").removeClass("hidden");
-          },
-          202: function() {
-            setTimeout(function() { pollResponse(res); }, 1000);
-          },
-          500: function() {
-            loader.addClass("hidden");
-            $("#error").removeClass("hidden");
-          }
-        }
-      });
-    }
   });
+
   function downloadUrl(fileName) {
     return "download/" + fileName;
   }
